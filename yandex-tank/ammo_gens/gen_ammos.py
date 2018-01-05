7cbdc703e22b961da53fdd0d61ad2b7a2bc54803
Anton Lamtev <antonlamtev@gmail.com>
2018-01-05 18:02:10 +0300

    fix

--- yandex-tank/ammo_gens/gen_ammos.py
+++ yandex-tank/ammo_gens/gen_ammos.py
@@ -13,12 +13,12 @@ if not os.path.exists(directory):
     os.makedirs(directory)
 os.chdir(directory)
 
-for method in methods:
+for mthd in methods:
     for rf in rfs:
         for n in ns:
             for ow in ows:
-                os.system('python gen_data.py {m} {rf} {n} {ow}'.format(m=method, rf=rf, n=n, ow=ow))
+                os.system('python ../ammo_gens/gen_data.py {m} {rf} {n} {ow}'.format(m=mthd, rf=rf, n=n, ow=ow))
 
 for f in os.listdir('.'):
     newf = f.replace('data', 'ammo')
-    os.system('cat {f} | python make_ammo.py > {newf}.txt'.format(f=f, newf=newf))
+    os.system('cat {f} | python ../ammo_gens/make_ammo.py > {newf}.txt'.format(f=f, newf=newf))
--- yandex-tank/ammo_gens/gen_data.py
+++ yandex-tank/ammo_gens/gen_data.py
@@ -9,28 +9,28 @@ def main():
         sys.stdout.write('Invalid number of arguments')
         return
 
-    method: str = sys.argv[1].upper()
+    mthd: str = sys.argv[1]
     rf: str = sys.argv[2]
     n: str = sys.argv[3]
     ow: bool = sys.argv[4] == '+'
 
     rfstr = rf.replace('/', '')
     owstr = '_ow' if ow else ''
-    file = open('data_{method}_{rf}_{n}{ow}'.format(method=method, rf=rfstr, n=n, ow=owstr), 'w')
+    file = open('data_{mthd}_{rf}_{n}{ow}'.format(method=mthd, rf=rfstr, n=n, ow=owstr), 'w')
 
-    if method == 'PUTGET':
+    if mthd == 'PUTGET':
         N = int(int(n) / 2)
         ow_n_times = 2 if ow else 1
         N = int(N / 2) if ow else N
         for i in range(0, N):
             ID = i
             for j in range(0, ow_n_times):
-                method = 'PUT'
+                mthd = 'PUT'
                 body = '||hello!! yandex-tank load test!!! gfsdgwergiwj4ofrgjoi4jgedfghweiorh'
-                file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=method, id=ID, rf=rf, body=body))
-                method = 'GET'
+                file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=mthd, id=ID, rf=rf, body=body))
+                mthd = 'GET'
                 body = ''
-                file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=method, id=ID, rf=rf, body=body))
+                file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=mthd, id=ID, rf=rf, body=body))
         return
 
     N = int(n)
@@ -39,11 +39,11 @@ def main():
     for i in range(0, N):
         ID = i
         for j in range(0, ow_n_times):
-            if method == 'PUT':
+            if mthd == 'PUT':
                 body = '||hello!! yandex-tank load test!!! gfsdgwergiwj4ofrgjoi4jgedfghweiorh'
             else:
                 body = ''
-            file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=method, id=ID, rf=rf, body=body))
+            file.write('{m}||/v0/entity?id={id}&replicas={rf}||tag{body}\n'.format(m=mthd, id=ID, rf=rf, body=body))
 
 
 if __name__ == "__main__":
